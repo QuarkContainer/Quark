3fa19c47df03f714d7cb83ba9eea99aafa60ddf2
brad <sunyiln728@gmail.com>
2021-04-23 06:23:02 -0700

    add more java test

--- test/java/helloworld.java
+++ test/java/helloworld.java
@@ -1,12 +1,20 @@
 /* This is a simple Java program. 
 FileName : "HelloWorld.java". */
-class HelloWorld 
-{ 
-	// Your program begins with a call to main(). 
-	// Prints "Hello, World" to the terminal window. 
-	public static void main(String args[]) 
-	{ 
-		System.out.println("Hello, World"); 
-	} 
-} 
+/*class HelloWorld
+{
+	// Your program begins with a call to main().
+	// Prints "Hello, World" to the terminal window.
+	public static void main(String args[])
+	{
+		System.out.println("Hello, World");
+	}
+}*/
 
+class HelloWorld {
+	int x = 5;
+
+	public static void main(String[] args) {
+		HelloWorld myObj = new HelloWorld();
+		System.out.println(myObj.x);
+	}
+}
