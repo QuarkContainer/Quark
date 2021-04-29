b485961571ee662bd504344aa90518f49cb8ea44
brad <sunyiln728@gmail.com>
2021-04-29 07:10:17 -0700

    add gettimeofday test

---
+++ test/c/gettimeofday.c
@@ -0,0 +1,12 @@
+#include <sys/time.h>
+#include <stdio.h>
+
+int main() {
+  struct timeval current_time;
+  gettimeofday(&current_time, NULL);
+  printf("seconds : %ld\nmicro seconds : %ld",
+    current_time.tv_sec, current_time.tv_usec);
+
+  return 0;
+}
+}
