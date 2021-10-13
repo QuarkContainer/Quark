431168a0261c1f16b76bb110a37f48f35ffc61d8
brad <yulin.sun@quarksoft.io>
2021-10-13 14:30:22 -0700

    clean iothread waiting

--- qvisor/src/kvmlib/runc/runtime/vm.rs
+++ qvisor/src/kvmlib/runc/runtime/vm.rs
@@ -350,9 +350,6 @@ impl VirtualMachine {
                 return;
             }
 
-            //PerfGofrom(PerfType::QCall);
-            FD_NOTIFIER.WaitAndNotify(shareSpace, 0).unwrap();
-
             for _ in 0..10 {
                 for _ in 0..20 {
                     if shareSpace.ReadyOutputMsgCnt() > 0 {
@@ -376,7 +373,7 @@ impl VirtualMachine {
                 }
 
                 //error!("io thread sleep... shareSpace.ReadyOutputMsgCnt() = {}", shareSpace.ReadyOutputMsgCnt());
-                let _cnt = FD_NOTIFIER.WaitAndNotify(shareSpace, -1).unwrap();
+                FD_NOTIFIER.Wait();
                 //error!("io thread wake...");
 
                 if !IsRunning() {
--- qvisor/src/kvmlib/vmspace/hostfdnotifier.rs
+++ qvisor/src/kvmlib/vmspace/hostfdnotifier.rs
@@ -12,56 +12,36 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use spin::RwLock;
 use libc::*;
-use core::ops::Deref;
 
-use super::super::qlib::common::*;
-use super::super::qlib::linux_def::*;
-use super::super::qlib::ShareSpace;
-use super::VMSpace;
-
-pub struct FdNotifierInternal {
+pub struct HostFdNotifier {
     //eventfd which guest notify host for message
     pub eventfd: i32,
-}
-
-pub struct HostFdNotifier(RwLock<FdNotifierInternal>);
-
-impl Deref for HostFdNotifier {
-    type Target = RwLock<FdNotifierInternal>;
-
-    fn deref(&self) -> &RwLock<FdNotifierInternal> {
-        &self.0
-    }
 }
 
 impl HostFdNotifier {
     pub fn New() -> Self {
         let eventfd = unsafe {
-            //eventfd(0, EFD_CLOEXEC | EFD_NONBLOCK)
-            eventfd(0, EFD_CLOEXEC)
+             eventfd(0, EFD_CLOEXEC)
         };
 
         if eventfd == -1 {
             panic!("FdNotifier::New create eventfd fail, error is {}", errno::errno().0);
         }
 
-        let internal = FdNotifierInternal {
+        return Self {
             eventfd: eventfd,
         };
-
-        return Self(RwLock::new(internal))
     }
 
     pub fn Eventfd(&self) -> i32 {
-        return self.read().eventfd;
+        return self.eventfd;
     }
 
     pub fn Notify(&self)  {
         let data: u64 = 1;
         let ret = unsafe {
-            write(self.read().eventfd, &data as *const _ as *const c_void, 8)
+            write(self.eventfd, &data as *const _ as *const c_void, 8)
         };
 
         if ret == -1 {
@@ -70,62 +50,16 @@ impl HostFdNotifier {
         }
     }
 
-    pub const MAX_EVENTS: usize = 128;
-    pub fn WaitAndNotify(&self, shareSpace: &'static ShareSpace, timeout: i32) -> Result<i32> {
-        let waitTime = if timeout == -1 { // blocked
-            //shareSpace.WaitInHost();
-            if shareSpace.ReadyAsyncMsgCnt() > 0 {
-                0
-            } else {
-                -1
-            }
-            //10
-        } else {
-            timeout
+    pub fn Wait(&self) {
+        let eventfd = self.eventfd;
+        let mut data : u64 = 0;
+        let ret = unsafe {
+            libc::read(eventfd, &mut data as * mut _ as *mut libc::c_void, 8)
         };
 
-        let eventfd = self.read().eventfd;
-        let mask : EventMask = EVENT_IN;
-        let mut e = pollfd {
-            fd: eventfd,
-            events: mask as i16,
-            revents: 0,
-        };
-
-        loop {
-            if waitTime >= 0 {
-                let ret = unsafe {
-                    poll(&mut e, 1, waitTime)
-                };
-
-                let ret = VMSpace::GetRet(ret as i64) as i32;
-                // Interrupted by signal, try again.
-                if ret == -SysErr::EINTR {
-                    continue;
-                }
-
-                // error happen
-                if ret < 0 {
-                    panic!("WaitAndNotify waitfail with error {}", ret);
-                }
-
-                // timeout
-                if ret == 0 {
-                    return Ok(0)
-                }
-            }
-
-            // eventfd is triggered
-            let mut data : u64 = 0;
-            let ret = unsafe {
-                libc::read(eventfd, &mut data as * mut _ as *mut libc::c_void, 8)
-            };
-
-            if ret < 0 {
-                panic!("WaitAndNotify... eventfd is {}, errno is {}",
-                       eventfd, errno::errno().0);
-            }
-            return Ok(1)
+        if ret < 0 {
+            panic!("WaitAndNotify... eventfd is {}, errno is {}",
+                   eventfd, errno::errno().0);
         }
     }
 
