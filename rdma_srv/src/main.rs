39ea951e9ab9cf8774119edfc38ef2e67b05f698
brad <yulin.sun@quarksoft.io>
2022-02-27 18:50:54 -0800

    initial add rdma_srv

---
+++ rdma_srv/src/main.rs
@@ -0,0 +1,78 @@
+// Copyright (c) 2021 Quark Container Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#![allow(dead_code)]
+#![allow(non_snake_case)]
+#![allow(deref_nullptr)]
+#![feature(proc_macro_hygiene)]
+#![feature(naked_functions)]
+#![allow(bare_trait_objects)]
+#![feature(map_first_last)]
+#![allow(non_camel_case_types)]
+#![feature(llvm_asm)]
+#![allow(deprecated)]
+#![feature(thread_id_value)]
+#![allow(dead_code)]
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+#![feature(core_intrinsics)]
+
+extern crate alloc;
+extern crate bit_field;
+extern crate core_affinity;
+extern crate errno;
+
+#[macro_use]
+extern crate serde_derive;
+extern crate cache_padded;
+extern crate serde;
+extern crate serde_json;
+
+#[macro_use]
+extern crate clap;
+
+#[macro_use]
+extern crate scopeguard;
+
+#[macro_use]
+extern crate lazy_static;
+
+extern crate libc;
+extern crate spin;
+extern crate x86_64;
+#[macro_use]
+extern crate log;
+extern crate caps;
+extern crate fs2;
+extern crate regex;
+extern crate simplelog;
+extern crate tabwriter;
+
+#[macro_use]
+pub mod print;
+
+#[macro_use]
+pub mod asm;
+
+pub mod qlib;
+pub mod rdma_conn;
+pub mod rdma_channel;
+pub mod kernel_def;
+
+use self::qlib::ShareSpaceRef;
+pub static SHARE_SPACE: ShareSpaceRef = ShareSpaceRef::New();
+
+fn main() {
+    println!("Hello, world!");
+}
