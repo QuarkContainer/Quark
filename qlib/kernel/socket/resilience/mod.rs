c920070f9d948974db901cc410549070d3100a65
brad <yulin.sun@quarksoft.io>
2023-01-13 09:25:28 -0800

    resilience socket#3

--- qlib/kernel/socket/resilience/mod.rs
+++ qlib/kernel/socket/resilience/mod.rs
@@ -12,5 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub mod message;
+pub mod user_msg;
 pub mod rsocket;
--- qlib/kernel/socket/resilience/user_msg.rs
+++ qlib/kernel/socket/resilience/user_msg.rs
@@ -17,13 +17,13 @@ use alloc::string::String;
 
 use crate::qlib::bytestream::*;
 use crate::qlib::common::*;
+use crate::qlib::linux_def::*;
 
 #[repr(u8)]
 pub enum UserMsgType {
     UserFuncCall = 1,
     UserFuncResp
 }
-
 
 // serialize format
 // len: u32
@@ -33,65 +33,67 @@ pub enum UserMsg {
     UserFuncCall(UserFuncCall),
     UserFuncResp(UserFuncResp),
 }
-/*
-impl RingBufIO for UserMsg {
+
+impl MessageIO for UserMsg {
     // data size, used for write check
     fn Size(&self) -> usize {
         match self {
             UserMsg::UserFuncCall(msg) => {
-                return 4 + 1 + msg.Size()
+                // msg type + msgboday
+                return 1 + msg.Size()
             }
             UserMsg::UserFuncResp(msg) => {
-                return 4 + 1 + msg.Size()
+                // msg type + msgboday
+                return 1 + msg.Size()
             }
         }
     }
 
     // read obj, return <Obj, whether trigger>
-    fn Read(buf: &mut ByteStreamIntern) -> Result<(Self, bool)> {
-        let (userData , trigger) = buf.readObj::<u64>()?;
+    fn Read(buf: &mut SocketBufIovs) -> Result<Self> {
+        let msgType = buf.ReadObj::<u8>()?;
 
-        let (namelen, _) = buf.readObj::<u16>()?;
-        let (name, _)= buf.readString(namelen as usize)?;
-
-        let (buflen , _) = buf.readObj::<u16>()?;
-        let (payload, _) = buf.readVec(buflen as usize)?;
-
-        let obj = Self {
-            userdata: userData,
-            funcName: name,
-            payload: payload,
-        };
-
-        return Ok((obj, trigger))
+        match msgType {
+            x if x == UserMsgType::UserFuncCall as u8 => {
+                let msg = UserFuncCall::Read(buf)?;
+                return Ok(Self::UserFuncCall(msg))
+            }
+            x if x == UserMsgType::UserFuncResp as u8 => {
+                let msg = UserFuncResp::Read(buf)?;
+                return Ok(Self::UserFuncResp(msg))
+            }
+            _ => return Err(Error::SysError(SysErr::EINVAL))
+        }
     }
     
     // write obj, return <whether trigger>
-    fn Write(&self, buf: &mut ByteStreamIntern) -> Result<bool> {
-        let size = self.Size();
-        let bufSize = buf.AvailableSpace();
-        if size < bufSize {
-            return Err(Error::NoEnoughSpace);
+    fn Write(&self, buf: &mut SocketBufIovs) -> Result<()> {
+        match self {
+            Self::UserFuncCall(msg) => {
+                buf.WriteObj(&(UserMsgType::UserFuncCall as u8))?;
+                msg.Write(buf)?;
+            }
+            Self::UserFuncResp(msg) => {
+                buf.WriteObj(&(UserMsgType::UserFuncResp as u8))?;
+                msg.Write(buf)?;
+            }
         }
 
-        buf.writeObj(&self.userdata)?;
-        buf.writeObj(&(self.funcName.len() as u16))?;
-        buf.writeSlice(self.funcName.as_bytes())?;
-        buf.writeObj(&(self.payload.len() as u16))?;
-        return buf.writeSlice(&self.payload);
+        return Ok(())
     }
 }
- */
+ 
 pub struct UserFuncCall {
-    userdata: u64,
-    // <len: u16, bytes: [u8]>
-    funcName: String,
+    pub userdata: u64,
     
     // <len: u16, bytes: [u8]>
-    payload: Vec<u8>, 
+    pub funcName: String,
+    
+    // <len: u16, bytes: [u8]>
+    pub payload: Vec<u8>, 
 }
 
-impl RingBufIO for UserFuncCall {
+impl MessageIO for UserFuncCall {
     // data size, used for write check
     fn Size(&self) -> usize {
         let mut size = 8;
@@ -101,10 +103,7 @@ impl RingBufIO for UserFuncCall {
     }
 
     // read obj, return <Obj, whether trigger>
-    fn Read(bs: &mut ByteStreamIntern) -> Result<(Self, bool)> {
-        bs.PrepareDataIovs();
-        let buf = &mut bs.dataIovs;
-
+    fn Read(buf: &mut SocketBufIovs) -> Result<Self> {
         let userData = buf.ReadObj::<u64>()?;
 
         let namelen = buf.ReadObj::<u16>()?;
@@ -119,82 +118,62 @@ impl RingBufIO for UserFuncCall {
             payload: payload,
         };
 
-        let trigger = bs.Consume(obj.Size());
-
-        return Ok((obj, trigger))
+        return Ok(obj)
     }
     
     // write obj, return <whether trigger>
-    fn Write(&self, bs: &mut ByteStreamIntern) -> Result<bool> {
-        bs.PrepareSpaceIovs();
-        let buf = &mut bs.spaceiovs;
-
-        let size = self.Size();
-        let bufSize = buf.Count();
-        if size < bufSize {
-            return Err(Error::NoEnoughSpace);
-        }
-
+    fn Write(&self, buf: &mut SocketBufIovs) -> Result<()> {
         buf.WriteObj(&self.userdata)?;
         buf.WriteObj(&(self.funcName.len() as u16))?;
         buf.WriteSlice(self.funcName.as_bytes())?;
         buf.WriteObj(&(self.payload.len() as u16))?;
         buf.WriteSlice(&self.payload)?;
 
-        let trigger = bs.Produce(size);
-        return Ok(trigger)
+        return Ok(())
     }
 }
 
 
 pub struct UserFuncResp {
-    userdata: u64,
-    payload: Vec<u8>,
+    pub userdata: u64,
+    pub sessionId: u64,
+    pub payload: Vec<u8>,
 }
 
-impl RingBufIO for UserFuncResp {
+impl MessageIO for UserFuncResp {
     // data size, used for write check
     fn Size(&self) -> usize {
         let mut size = 8;
+        size += 8;
+
+        // 2 is the len of payload
         size += 2 + self.payload.len();
         return size
     }
 
     // read obj, return <Obj, whether trigger>
-    fn Read(bs: &mut ByteStreamIntern) -> Result<(Self, bool)> {
-        bs.PrepareDataIovs();
-        let buf = &mut bs.dataIovs;
-
+    fn Read(buf: &mut SocketBufIovs) -> Result<Self> {
         let userData = buf.ReadObj::<u64>()?;
+        let sessionId = buf.ReadObj::<u64>()?;
         let buflen = buf.ReadObj::<u16>()?;
         let payload = buf.ReadVec(buflen as usize)?;
 
         let obj = Self {
             userdata: userData,
+            sessionId: sessionId,
             payload: payload,
         };
 
-        let trigger = bs.Consume(obj.Size());
-
-        return Ok((obj, trigger))
+        return Ok(obj)
     }
     
     // write obj, return <whether trigger>
-    fn Write(&self, bs: &mut ByteStreamIntern) -> Result<bool> {
-        bs.PrepareSpaceIovs();
-        let buf = &mut bs.spaceiovs;
-
-
-        let size = self.Size();
-        let bufSize = buf.Count();
-        if size < bufSize {
-            return Err(Error::NoEnoughSpace);
-        }
-
+    fn Write(&self, buf: &mut SocketBufIovs) -> Result<()> {
         buf.WriteObj(&self.userdata)?;
+        buf.WriteObj(&self.sessionId)?;
         buf.WriteObj(&(self.payload.len() as u16))?;
         buf.WriteSlice(&self.payload)?;
 
-        return Ok(bs.Produce(size));
+        return Ok(());
     }
 }
