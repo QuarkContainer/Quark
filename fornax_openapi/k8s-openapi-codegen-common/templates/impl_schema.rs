62a008bf5aaf88edd8451119c2206783c3359eb1
brad <yulin.sun@quarksoft.io>
2023-04-02 03:47:26 -0700

    remove k8s files

--- fornax_openapi/k8s-openapi-codegen-common/templates/impl_schema.rs
+++
@@ -1,9 +0,0 @@
-
-{cfg}impl{type_generics_impl} {local}schemars::JsonSchema for {type_name}{type_generics_type}{type_generics_where} {{
-    fn schema_name() -> String {{
-        {definition_path:?}.to_owned()
-    }}
-
-    fn json_schema(__gen: &mut {local}schemars::gen::SchemaGenerator) -> {local}schemars::schema::Schema {{
-{schema}    }}
-}}
--- fornax_openapi/k8s-openapi-codegen-common/templates/struct_deep_merge.rs
+++
@@ -1,5 +0,0 @@
-
-impl{type_generics_type} {local}DeepMerge for {type_name}{type_generics_type}{type_generics_where} {{
-    fn merge_from(&mut self, other: Self) {{
-{merge_body}    }}
-}}
