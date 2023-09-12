fc978734cedc70fc1c6601ba39240eeb7aefbd34
CharlyYu <jiayouyu1185@gmail.com>
2023-09-12 15:53:47 +0000

    aarch64: fix qvisor compilation
    Signed-off-by: CharlyYu <jiayouyu1185@gmail.com>

---
+++ qvisor/src/kvm_vcpu_aarch64.rs
@@ -0,0 +1,12 @@
+use super::qlib::common::Result;
+use crate::KVMVcpu;
+
+impl KVMVcpu {
+    pub fn run(&self, tgid: i32) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn dump(&self) -> Result<()> {
+        Ok(())
+    }
+}
