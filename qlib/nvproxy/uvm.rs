8e0dcb8c632252e9c738723b22b979987b5f0144
quarksoft <yulin.sun@quarksoft.io>
2023-10-12 08:48:28 -0700

    add uvmfd ioctl

--- qlib/nvproxy/uvm.rs
+++ qlib/nvproxy/uvm.rs
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use super::nvgpu::*;
+use super::{nvgpu::*, uvmfd::UvmFileOptions};
 
 pub trait HasRMCtrlFD  {
     fn GetRMCtrlFD (&self) -> i32;
@@ -21,27 +21,28 @@ pub trait HasRMCtrlFD  {
 // UVM ioctl commands.
 
 // From kernel-open/nvidia-uvm/uvm_linux_ioctl.h:
-pub const UVM_INITIALIZE   : u64 = 0x30000001;
-pub const UVM_DEINITIALIZE : u64 = 0x30000002;
+pub const UVM_INITIALIZE   : u32 = 0x30000001;
+pub const UVM_DEINITIALIZE : u32 = 0x30000002;
 
 // From kernel-open/nvidia-uvm/uvm_ioctl.h:
-pub const UVM_CREATE_RANGE_GROUP             : u64 = 23;
-pub const UVM_DESTROY_RANGE_GROUP            : u64 = 24;
-pub const UVM_REGISTER_GPU_VASPACE           : u64 = 25;
-pub const UVM_UNREGISTER_GPU_VASPACE         : u64 = 26;
-pub const UVM_REGISTER_CHANNEL               : u64 = 27;
-pub const UVM_UNREGISTER_CHANNEL             : u64 = 28;
-pub const UVM_MAP_EXTERNAL_ALLOCATION        : u64 = 33;
-pub const UVM_FREE                           : u64 = 34;
-pub const UVM_REGISTER_GPU                   : u64 = 37;
-pub const UVM_UNREGISTER_GPU                 : u64 = 38;
-pub const UVM_PAGEABLE_MEM_ACCESS            : u64 = 39;
-pub const UVM_MAP_DYNAMIC_PARALLELISM_REGION : u64 = 65;
-pub const UVM_ALLOC_SEMAPHORE_POOL           : u64 = 68;
-pub const UVM_VALIDATE_VA_RANGE              : u64 = 72;
-pub const UVM_CREATE_EXTERNAL_RANGE          : u64 = 73;
+pub const UVM_CREATE_RANGE_GROUP             : u32 = 23;
+pub const UVM_DESTROY_RANGE_GROUP            : u32 = 24;
+pub const UVM_REGISTER_GPU_VASPACE           : u32 = 25;
+pub const UVM_UNREGISTER_GPU_VASPACE         : u32 = 26;
+pub const UVM_REGISTER_CHANNEL               : u32 = 27;
+pub const UVM_UNREGISTER_CHANNEL             : u32 = 28;
+pub const UVM_MAP_EXTERNAL_ALLOCATION        : u32 = 33;
+pub const UVM_FREE                           : u32 = 34;
+pub const UVM_REGISTER_GPU                   : u32 = 37;
+pub const UVM_UNREGISTER_GPU                 : u32 = 38;
+pub const UVM_PAGEABLE_MEM_ACCESS            : u32 = 39;
+pub const UVM_MAP_DYNAMIC_PARALLELISM_REGION : u32 = 65;
+pub const UVM_ALLOC_SEMAPHORE_POOL           : u32 = 68;
+pub const UVM_VALIDATE_VA_RANGE              : u32 = 72;
+pub const UVM_CREATE_EXTERNAL_RANGE          : u32 = 73;
 
 // UVM_INITIALIZE_PARAMS
+#[derive(Debug, Clone, Copy)]
 #[repr(C)]
 pub struct UvmInitializeParams {
     pub flags    : u64, 
@@ -50,9 +51,10 @@ pub struct UvmInitializeParams {
 }
 
 // UVM_INITIALIZE_PARAMS flags, from kernel-open/nvidia-uvm/uvm_types.h.
-pub const UVM_INIT_FLAGS_MULTI_PROCESS_SHARING_MODE : i32 = 0x2;
+pub const UVM_INIT_FLAGS_MULTI_PROCESS_SHARING_MODE : u64 = 0x2;
 
 // UVM_CREATE_RANGE_GROUP_PARAMS
+#[derive(Debug, Clone, Copy)]
 #[repr(C)]
 pub struct UvmCreateRangeGroupParams {
 	pub rangeGroupID : u64,
@@ -62,6 +64,7 @@ pub struct UvmCreateRangeGroupParams {
 
 
 // UVM_DESTROY_RANGE_GROUP_PARAMS
+#[derive(Debug, Clone, Copy)]
 #[repr(C)]
 pub struct UvmDestroyRangeGroupParams {
 	pub rangeGroupID : u64,
@@ -70,6 +73,7 @@ pub struct UvmDestroyRangeGroupParams {
 }
 
 // UVM_REGISTER_GPU_VASPACE_PARAMS
+#[derive(Debug, Clone, Copy)]
 #[repr(C)]
 pub struct UvmRegisterGpuVaspaceParams {
     pub GPUUUID  : [u8; 16],
@@ -90,6 +94,7 @@ impl HasRMCtrlFD  for UvmRegisterGpuVaspaceParams {
 }
 
 // UVM_REGISTER_CHANNEL_PARAMS
+#[derive(Debug, Clone, Copy)]
 #[repr(C)]
 pub struct UvmRegisterChannelParams {
 	pub GPUUUID  : [u8; 16],
@@ -114,6 +119,7 @@ impl HasRMCtrlFD  for UvmRegisterChannelParams {
 }
 
 // UVM_UNREGISTER_CHANNEL_PARAMS
+#[derive(Debug, Clone, Copy)]
 #[repr(C)]
 pub struct UvmUnregisterChannelParams {
     pub GPUUUID  : [u8; 16],
@@ -123,6 +129,7 @@ pub struct UvmUnregisterChannelParams {
 }
 
 // UVM_MAP_EXTERNAL_ALLOCATION_PARAMS
+#[derive(Debug, Clone, Copy)]
 #[repr(C)]
 pub struct UvmMapExternalAllocationParams {
     pub base               : u64,
@@ -147,6 +154,7 @@ impl HasRMCtrlFD  for UvmMapExternalAllocationParams {
 }
 
 // UVM_FREE_PARAMS
+#[derive(Debug, Clone, Copy)]
 #[repr(C)]
 pub struct UvmFreeParams {
 	pub base     : u64,
@@ -156,6 +164,7 @@ pub struct UvmFreeParams {
 }
 
 // UVM_REGISTER_GPU_PARAMS
+#[derive(Debug, Clone, Copy)]
 #[repr(C)]
 pub struct UvmRegisterGpuParams {
 	pub GPUUUID     : [u8; 16],
@@ -179,6 +188,7 @@ impl HasRMCtrlFD  for UvmRegisterGpuParams {
 }
 
 // UVM_UNREGISTER_GPU_PARAMS
+#[derive(Debug, Clone, Copy)]
 #[repr(C)]
 pub struct UvmUnregisterGpuParams {
 	pub GPUUUID     : [u8; 16],
@@ -186,6 +196,7 @@ pub struct UvmUnregisterGpuParams {
 }
 
 // UVM_PAGEABLE_MEM_ACCESS_PARAMS
+#[derive(Debug, Clone, Copy)]
 #[repr(C)]
 pub struct UvmPageableMemAccessParams {
 	pub pageableMemAccess : u8,
@@ -194,6 +205,7 @@ pub struct UvmPageableMemAccessParams {
 }
 
 // UVM_MAP_DYNAMIC_PARALLELISM_REGION_PARAMS
+#[derive(Debug, Clone, Copy)]
 #[repr(C)]
 pub struct UvmMapDynamicParallelismRegionParams {
     pub base     : u64,
@@ -204,6 +216,7 @@ pub struct UvmMapDynamicParallelismRegionParams {
 }
 
 // UVM_ALLOC_SEMAPHORE_POOL_PARAMS
+#[derive(Debug, Clone, Copy)]
 #[repr(C)]
 pub struct UvmAllocSemaphorePoolParams {
 	pub base               : u64,
@@ -215,6 +228,7 @@ pub struct UvmAllocSemaphorePoolParams {
 }
 
 // UVM_VALIDATE_VA_RANGE_PARAMS
+#[derive(Debug, Clone, Copy)]
 #[repr(C)]
 pub struct UvmValidateVaRangeParams {
 	pub base     : u64,
@@ -224,6 +238,7 @@ pub struct UvmValidateVaRangeParams {
 }
 
 // UVM_CREATE_EXTERNAL_RANGE_PARAMS
+#[derive(Debug, Clone, Copy)]
 #[repr(C)]
 pub struct UvmCreateExternalRangeParams {
 	pub base     : u64,
@@ -237,6 +252,7 @@ pub struct UvmCreateExternalRangeParams {
 pub const UVM_MAX_GPUS : usize = NV_MAX_DEVICES;
 
 // UvmGpuMappingAttributes
+#[derive(Debug, Clone, Copy)]
 #[repr(C)]
 pub struct UvmGpuMappingAttributes {
 	pub GPUUUID            : [u8; 16],
@@ -246,3 +262,10 @@ pub struct UvmGpuMappingAttributes {
 	pub GPUElementBits     : u32,
 	pub GPUCompressionType : u32,
 }
+
+pub struct UvmIoctlState {
+	pub fd: UvmFileOptions,
+	pub cmd: u32,
+	pub ioctlParamsAddr: u64,
+}
+
