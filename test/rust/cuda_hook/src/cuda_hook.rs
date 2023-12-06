e800076bb1664bfbe8d651f61e4867afc8e9b662
Hardys Jin <hardyshongyijin@gmail.com>
2023-12-06 11:10:18 -0800

    Add cudaMalloc

--- test/rust/cuda_hook/src/cuda_hook.rs
+++ test/rust/cuda_hook/src/cuda_hook.rs
@@ -1,6 +1,6 @@
 use std::os::raw::{c_int, c_void};
 use std::ffi::CString;
-use cuda_runtime_sys::{dim3, cudaStream_t, cudaError_t};
+use cuda_runtime_sys::{dim3, cudaStream_t, cudaError_t, cudaMemcpyKind};
 
 #[no_mangle]
 pub extern "C" fn cudaSetDevice(device: c_int) -> cudaError_t {
@@ -46,4 +46,38 @@ pub extern "C" fn cudaLaunchKernel(
         std::mem::transmute(libc::dlsym(handle, func_name.as_ptr()))
     };
     orig_func(func, grid_dim, block_dim, args, shared_mem, stream)
+}
+
+#[no_mangle]
+pub extern "C" fn cudaMalloc(
+        dev_ptr: *mut *mut c_void, 
+        size: usize
+    ) -> cudaError_t {
+    println!("Hijacked cudaMalloc(size:{})", size);
+
+    let lib = CString::new("libcudart.so").unwrap();
+    let handle = unsafe { libc::dlopen(lib.as_ptr(), libc::RTLD_LAZY) };    
+    let func_name = CString::new("cudaMalloc").unwrap();
+    let orig_func: extern "C" fn(*mut *mut c_void, usize) -> cudaError_t = unsafe {
+        std::mem::transmute(libc::dlsym(handle, func_name.as_ptr()))
+    };
+    orig_func(dev_ptr, size)
+}
+
+#[no_mangle]
+pub extern "C" fn cudaMemcpy(
+        dst: *mut c_void, 
+        src: *const c_void, 
+        count: usize, 
+        kind: cudaMemcpyKind
+    ) -> cudaError_t {
+    println!("Hijacked cudaMemcpy(count:{})", count);
+
+    let lib = CString::new("libcudart.so").unwrap();
+    let handle = unsafe { libc::dlopen(lib.as_ptr(), libc::RTLD_LAZY) };    
+    let func_name = CString::new("cudaMemcpy").unwrap();
+    let orig_func: extern "C" fn(*mut c_void, *const c_void, usize, cudaMemcpyKind) -> cudaError_t = unsafe {
+        std::mem::transmute(libc::dlsym(handle, func_name.as_ptr()))
+    };
+    orig_func(dst, src, count, kind)
 }
