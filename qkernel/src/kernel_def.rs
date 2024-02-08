daba2c6ac4cfd554a329ce767acc27583829800d
Tianhao <tianhao.wang2@mailbox.tu-dresden.de>
2024-02-08 01:32:37 +0100

    aarch64: control accessing user mem from EL1
    by setting and unsetting PAN (when feature supported). To enable cpu
    feature, add "+pan" in the features (aarch64-qkernel.json). Otherwise
    the enable_access_user() and set_access_user() simply return.

--- qkernel/src/kernel_def.rs
+++ qkernel/src/kernel_def.rs
@@ -49,6 +49,7 @@ use super::qlib::*;
 use super::syscalls::sys_file::*;
 use super::Kernel::HostSpace;
 
+
 use crate::GLOBAL_ALLOCATOR;
 
 impl IoUring {
@@ -512,4 +513,33 @@ pub fn IsKernel() -> bool {
 
 pub fn ReapSwapIn() {
     HostSpace::SwapIn();
+}
+
+/// enable access to EL0 memory from EL1 return the previous state
+/// true : enabled, false : disabled
+#[inline]
+pub fn enable_access_user() -> bool {
+    #[cfg(target_feature = "pan")]
+    {
+        unsafe {
+            // PAN==1 means access NOT allowed
+            let allow = !pan();
+            pan_set(false);
+            return allow;
+        }
+    }
+    // if PAN is not the case, accessing user memory is always enabled for EL1
+    return true;
+}
+
+/// reset access to EL0 memory from EL1 return the previous state
+/// true : enable false : disable
+#[inline]
+pub fn set_access_user(allow: bool) {
+    #[cfg(target_feature = "pan")]
+    {
+        unsafe {
+            pan_set(!allow);
+        }
+    }
 }
