152ad9880027b16fd66b8b87b3a6429326080f68
brad <yulin.sun@quarksoft.io>
2024-05-19 07:41:25 -0700

    add test ai program
    Signed-off-by: Tianhao Wang <shrik3@mailbox.org>

--- test/llama1b.py
+++ test/llama1b.py
@@ -1,6 +1,7 @@
 import numpy
 import os
 import time
+import sys
 os.environ['CURL_CA_BUNDLE'] = ''
 
 start = time.time()
@@ -18,24 +19,19 @@ model.to("cuda:0")
 print(model)
 tokenizer = AutoTokenizer.from_pretrained("TinyLlama/TinyLlama-1.1B-Chat-v1.0")
 #tokenizer = AutoTokenizer.from_pretrained("TinyLlama/TinyLlama-1.1B-Chat-v1.0", cache_dir="/model_weight")
-
 start2 = time.time()
-
-prompt = "Vancouver is a"
-inputs = tokenizer(prompt, return_tensors="pt").to("cuda:0")
-generate_ids = model.generate(inputs.input_ids, max_length=50, pad_token_id=tokenizer.eos_token_id)
-print(tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0])
-
-start3 = time.time()
-
-prompt = "Vancouver is a"
-inputs = tokenizer(prompt, return_tensors="pt").to("cuda:0")
-generate_ids = model.generate(inputs.input_ids, max_length=50, pad_token_id=tokenizer.eos_token_id)
-print(tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0])
-
-end = time.time()
 print("torch time: ", start1-start)
 print("model time: ", start2-start1)
-print("exec1 time: ", start3-start2)
-print("exec2 time: ", end-start3)
-print("e2e time: ", end-start)
+
+for i in range(1, 10):
+    #sys.stdin.read(1)
+    begin = time.time()
+
+    prompt = "Vancouver is a"
+    inputs = tokenizer(prompt, return_tensors="pt").to("cuda:0")
+    generate_ids = model.generate(inputs.input_ids, max_length=50, pad_token_id=tokenizer.eos_token_id)
+    print(tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0])
+    end = time.time()
+    print("exec",  i, "time: ", end - begin)
+
+
