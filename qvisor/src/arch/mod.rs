5de1fd4f06a21647c5bdae3ed341b7426a555e54
ch1337 <christo.bita@mailbox.tu-dresden.de>
2024-06-21 11:57:07 +0200

    vcpu refactor: add traits for vcpu
            - specify base functionality trait for vcpus
            - specify (first version) trait for confidentiality feature
              extension

--- qvisor/src/arch/mod.rs
+++ qvisor/src/arch/mod.rs
@@ -12,4 +12,48 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub mod x86_64;
+#[cfg(target_arch = "aarch64")]
+#[path = "./aarch64/mod.rs"]
+pub mod vm;
+
+#[cfg(target_arch = "x86_64")]
+#[path = "./x86_64/mod.rs"]
+pub mod vm;
+
+pub mod tee;
+
+use std::os::fd::RawFd;
+use kvm_ioctls::{Kvm, VcpuExit, VcpuFd, VmFd};
+use vm::vcpu::ArchVirtCpu;
+use crate::{qlib::common::Error, CCMode};
+
+pub trait VirtCpu {
+    fn new_vcpu(vcpu_id: usize, total_vcpus: usize, vm_fd: &VmFd, entry_addr: u64,
+        page_allocator_base_addr: Option<u64>, share_space_table_addr: Option<u64>,
+        auto_start: bool, stack_size: usize, kvm: Option<&Kvm>, conf_extension: CCMode)
+        -> Result<ArchVirtCpu, Error>;
+    fn initialize_sys_registers(&self) -> Result<(), Error>;
+    fn initialize_cpu_registers(&self) -> Result<(), Error>;
+    fn default_hypercall_handler(&self, hypercall: u16, data: &[u8],
+        arg0: u64, arg1: u64, arg2: u64, arg3: u64) -> Result<bool, Error>;
+    fn default_kvm_exit_handler(&self, kvm_exit: VcpuExit) -> Result<bool, Error>;
+    fn vcpu_run(&self, tgid: i32, _kvm_raw_fd: Option<RawFd>, _vm_raw_fd: Option<RawFd>)
+        -> Result<(), Error>;
+}
+
+pub trait ConfCompExtension: Send + Sync {
+    fn initialize_conf_extension(share_space_table_addr: Option<u64>,
+        page_allocator_base_addr: Option<u64>) -> Result<Box<dyn ConfCompExtension>, Error>
+        where Self: Sized;
+    fn set_sys_registers(&self, vcpu_fd: &VcpuFd) -> Result<(), Error>;
+    fn set_cpu_registers(&self, vcpu_fd: &VcpuFd) -> Result<(), Error>;
+    fn get_hypercall_arguments(&self, vcpu_fd: &VcpuFd, vcpu_id: usize)
+        -> Result<(u64, u64, u64, u64), Error>;
+    fn should_handle_kvm_exit(&self, kvm_exit: &VcpuExit) -> bool;
+    fn should_handle_hypercall(&self, hypercall: u16) -> bool;
+    fn handle_kvm_exit(&self, kvm_exit: &mut VcpuExit, vcpu_id: usize, _vm_fd: Option<&VmFd>)
+        -> Result<bool, Error>;
+    fn handle_hypercall(&self, hypercall: u16, data: &[u8], arg0: u64, arg1: u64, arg2: u64,
+        arg3: u64, vcpu_id: usize) -> Result<bool , Error>;
+    fn confidentiality_type(&self) -> CCMode;
+}
