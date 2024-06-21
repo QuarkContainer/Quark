30f635dee03f3e423f3bb55e0c6f42a3e0d95bbc
ch1337 <christo.bita@mailbox.tu-dresden.de>
2024-06-21 12:17:48 +0200

    x86_64: vcpu refactor: VirtCpu trait implementation

--- qvisor/src/arch/x86_64/mod.rs
+++ qvisor/src/arch/x86_64/mod.rs
@@ -12,4 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub mod context;
+//NOTE: Removed on future chores
+//pub mod context;
+pub mod vcpu;
+pub mod tee;
